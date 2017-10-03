005c1cac0f1278ba1bce48bfd1b47d603274ee98
Ricardo Dani <ricardodani@gmail.com>
2017-10-03 17:59:28 -0300

    Added `Planos Adicionais`, and admin

--- vcxsolutions_devtest/tarifas/admin.py
+++ vcxsolutions_devtest/tarifas/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from tarifas.models import Plan, SMSPlan, DataPlan
 
-# Register your models here.
+
+class PlanAdmin(admin.ModelAdmin):
+    list_display = ('name', 'data_pack', 'sms_pack', 'value')
+
+
+class SMSPlanAdmin(admin.ModelAdmin):
+    list_display = ('sms_pack', 'value')
+
+
+class DataPlanAdmin(admin.ModelAdmin):
+    list_display = ('data_pack', 'value')
+
+
+admin.site.register(Plan, PlanAdmin)
+admin.site.register(SMSPlan, SMSPlanAdmin)
+admin.site.register(DataPlan, DataPlanAdmin)
--- vcxsolutions_devtest/tarifas/models.py
+++ vcxsolutions_devtest/tarifas/models.py
@@ -6,7 +6,7 @@ class Plan(models.Model):
 
     name = models.CharField(
         verbose_name='Nome do Plano',
-        max_lenght=15
+        max_length=15
     )
 
     minutes_franchise = models.IntegerField(
@@ -27,7 +27,7 @@ class Plan(models.Model):
     @property
     def data_pack(self):
         return '{}{}'.format(self.data_pack_value, self.data_pack_unit)
-    data_pack.verbose_name = 'Pacote de dados'
+    data_pack.fget.short_description = 'Pacote de dados'
 
     sms_pack_size = models.PositiveIntegerField(
         verbose_name='Pacote de SMS',
@@ -36,11 +36,12 @@ class Plan(models.Model):
     @property
     def sms_pack(self):
         return '{} SMS'.format(self.sms_pack_size)
-    sms_pack.verbose_name = 'Pacote de SMS'
+    sms_pack.fget.short_description = 'Pacote de SMS'
 
     value = models.DecimalField(
         verbose_name='Valor',
         decimal_places=2,
+        max_digits=8
     )
 
     def __str__(self):
@@ -48,3 +49,65 @@ class Plan(models.Model):
 
     class Meta:
         verbose_name = 'Plano'
+
+
+class SMSPlan(models.Model):
+    '''Plano de SMS Adicional'''
+
+    sms_pack_size = models.PositiveIntegerField(
+        verbose_name='Pacote de SMS',
+        null=True,
+        blank=True
+    )
+
+    @property
+    def sms_pack(self):
+        if self.sms_pack_size:
+            return '{} SMS'.format(self.sms_pack_size)
+        else:
+            return 'SMS ILIMITADOS'
+    sms_pack.fget.short_description = 'Pacote de SMS Adicional'
+
+    value = models.DecimalField(
+        verbose_name='Valor',
+        decimal_places=2,
+        max_digits=8
+    )
+
+    def __str__(self):
+        return self.sms_pack
+
+    class Meta:
+        verbose_name = 'Plano de SMS Adicional'
+        verbose_name_plural = 'Planos de SMS Adicionais'
+
+
+class DataPlan(models.Model):
+    '''Plano de Dados Adicional'''
+
+    data_pack_unit = models.CharField(
+        verbose_name='Pacote de dados (unidade)',
+        choices=(('MB', 'Megabyte'), ('GB', 'Gigabyte')),
+        max_length=2
+    )
+
+    data_pack_value = models.PositiveIntegerField(
+        verbose_name='Pacote de dados (valor)',
+    )
+
+    @property
+    def data_pack(self):
+        return '{}{}'.format(self.data_pack_value, self.data_pack_unit)
+    data_pack.fget.short_description = 'Pacote de dados Adicional'
+
+    value = models.DecimalField(
+        verbose_name='Valor',
+        decimal_places=2,
+        max_digits=8
+    )
+
+    def __str__(self):
+        return self.data_pack
+
+    class Meta:
+        verbose_name = 'Plano de Dados Adicional'
