24db26d2a57895111905f09484789f63a3a9e472
Ricardo Dani <ricardodani@gmail.com>
2017-10-09 23:36:44 -0300

    Done with SMS

--- vcxsolutions_devtest/tarifas/views.py
+++ vcxsolutions_devtest/tarifas/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import TemplateView
-from tarifas.models import Plan, DataPlan
+from tarifas.models import Plan, DataPlan, SMSPlan
 
 
 class PackageSet(object):
@@ -39,44 +39,73 @@ class Package(object):
     based on the input parameters as min_mb and min_sms.
     '''
 
-    data_plans = []
-
     def get_data_plans(self):
-        if self.mb_left <= 0:
-            return self.data_plans
-        cheapest = DataPlan.objects.filter(
-            data_mb__gte=self.mb_left
-        ).order_by('value').first()
-        if cheapest:
-            self.mb_left = 0
-            self.data_plans.append(cheapest)
-        else:
-            biggest = DataPlan.objects.filter(
-                data_mb__lt=self.mb_left
-            ).order_by('-data_mb').first()
-            if biggest:
-                self.mb_left -= biggest.data_mb
-                self.data_plans.append(biggest)
-            else:
+        data_plans = []
+        while self.mb_left > 0:
+            cheapest = DataPlan.objects.filter(
+                data_mb__gte=self.mb_left
+            ).order_by('value').first()
+            if cheapest:
                 self.mb_left = 0
-        self.get_data_plans()
+                data_plans.append(cheapest)
+                break
+            else:
+                biggest = DataPlan.objects.filter(
+                    data_mb__lt=self.mb_left
+                ).order_by('-data_mb').first()
+                if biggest:
+                    self.mb_left -= biggest.data_mb
+                    data_plans.append(biggest)
+                else:
+                    break
+        return data_plans
 
-    def get_additional_sms_plans(self):
-        if self.sms_left <= 0:
-            return []
+    def get_sms_plans(self):
         # get cheapest gte sms_left
         # if not, return infinite
         # elsewhere return biggest
+        sms_plans = []
+        if self.sms_left > 0:
+            cheapest = SMSPlan.objects.filter(
+                sms_pack_size__gte=self.sms_left,
+                unlimited=False
+            ).order_by('value').first()
+            if cheapest:
+                sms_plans.append(cheapest)
+            else:
+                unlimited = SMSPlan.objects.filter(unlimited=True).first()
+                if unlimited:
+                    sms_plans.append(unlimited)
+        return sms_plans
 
     @property
     def total_value(self):
-        return 1
+        value_data_plans = sum(plan.value for plan in self.data_plans)
+        return self.plan.value + value_data_plans
+
+    @property
+    def total_data(self):
+        mb_data_plans = sum(plan.data_mb for plan in self.data_plans)
+        total = self.plan.data_mb + mb_data_plans
+        if total >= 1024:
+            return '{0:.3g}GB'.format(total / 1024)
+        else:
+            return '{}MB'.format(total)
 
     def __init__(self, plan, min_mb, min_sms):
-        self.plan = plan
-        self.mb_left = min_mb - plan.data_mb
+        self.plan, self.min_mb, self.min_sms = plan, min_mb, min_sms
+        if min_mb > plan.data_mb:
+            self.mb_left = min_mb - plan.data_mb
+            self.data_plans = self.get_data_plans()
+        else:
+            self.data_plans = []
+
+        if min_sms > plan.sms_pack_size:
+            self.sms_left = min_sms - plan.sms_pack_size
+            self.sms_plans = self.get_sms_plans()
+        else:
+            self.sms_plans = []
         self.sms_left = min_sms - plan.sms_pack_size
-        self.data_plans = self.get_data_plans()
 
 
 class Home(TemplateView):
